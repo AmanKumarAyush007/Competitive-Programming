045317459d125353d59d9bd81ea7967d74fa2877
AmanKumarAyush007 <amanedu55699@gmail.com>
2026-04-17 16:46:00 +0530

    2026-04-17 16:46:00

---
+++ CF/E_Increasing_Subsequences.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h" 
+#else
+#define debug(x...)
+#endif
+#define int              int64_t
+#define ff               first
+#define ss               second
+#define pb               push_back
+#define inf              (int)1e18
+#define nl               '\n'
+#define all(a)           (a).begin(),(a).end()
+#define rall(a)          (a).rbegin(),(a).rend()
+#define sm(v)            accumulate(all(v),0LL)
+#define inp(v)           for(auto& x : v) cin >> x;
+#define setbit(x)        __builtin_popcountll(x)
+#define lg(x)            (63 - __builtin_clzll(x)) //log base 2
+#define prefixsum(a)     partial_sum(all(a), (a).begin());
+#define suffixsum(a)     partial_sum(rall(a), (a).rbegin());
+
+
+
+void solve(){
+    int n;
+    cin >> n;
+
+    vector<int> ans;
+
+    for(int i = 63; i > 0; i--){
+        if(n & (1LL<<i)){
+            if(!ans.size()) {
+                for(int j = 1; j <= i; j++){
+                    ans.pb(j);
+                }
+            }
+            else ans.pb(i+1);
+        }
+    }
+
+    if(n&1) ans.pb(0);
+
+    cout << ans.size() << nl;
+    for(auto &i : ans) cout << i << " ";
+    cout << nl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
