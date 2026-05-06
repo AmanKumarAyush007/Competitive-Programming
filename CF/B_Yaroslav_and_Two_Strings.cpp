8720e61d2606f435b355bf4ceff9b904279e24a2
AmanKumarAyush007 <amanedu55699@gmail.com>
2026-05-06 16:33:55 +0530

    2026-05-06 16:33:55

--- CF/B_Yaroslav_and_Two_Strings.cpp
+++ CF/B_Yaroslav_and_Two_Strings.cpp
@@ -1,69 +1,82 @@
 #include<bits/stdc++.h>
-#define int long long
 using namespace std;
 
-const int MOD = 1e9 + 7;
-const int N = 1e5 + 5;
+#ifndef ONLINE_JUDGE
+#include "debug.h" 
+#else
+#define debug(x...)
+#endif
+#define int              int64_t
+#define ff               first
+#define ss               second
+#define pb               push_back
+#define inf              (int)1e18
+#define nl               '\n'
+#define all(a)           (a).begin(),(a).end()
+#define rall(a)          (a).rbegin(),(a).rend()
+#define sm(v)            accumulate(all(v),0LL)
+#define inp(v)           for(auto& x : v) cin >> x;
+#define setbit(x)        __builtin_popcountll(x)
+#define lg(x)            (63 - __builtin_clzll(x)) //log base 2
+#define prefixsum(a)     partial_sum(all(a), (a).begin());
+#define suffixsum(a)     partial_sum(rall(a), (a).rbegin());
 
-string s, t;
+const int N = 1e5+5;
+const int mod = 1000000007;
 int dp[N][2][2];
+int n;
+string s, t;
 
-int32_t main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+int rec(int i, bool x, bool y){
+    if(i >= n) return x&y;
 
-    int n;
-    cin >> n >> s >> t;
+    if(dp[i][x][y] != -1) return dp[i][x][y];
 
-    dp[0][0][0] = 1;
-
-    for(int i = 0; i < n; i++){
-        for(int condA = 0; condA < 2; condA++){
-            for(int condB = 0; condB < 2; condB++){
-
-                int cur = dp[i][condA][condB];
-                if(cur == 0) continue;
-
-                if(s[i] == '?' && t[i] == '?'){
-                    // s > t
-                    dp[i+1][1][condB] = (dp[i+1][1][condB] + cur * 45) % MOD;
-
-                    // s < t
-                    dp[i+1][condA][1] = (dp[i+1][condA][1] + cur * 45) % MOD;
-
-                    // s == t
-                    dp[i+1][condA][condB] = (dp[i+1][condA][condB] + cur * 10) % MOD;
-                }
-                else if(s[i] == '?'){
-                    // s > t
-                    dp[i+1][1][condB] = (dp[i+1][1][condB] + cur * ('9' - t[i])) % MOD;
-
-                    // s < t
-                    dp[i+1][condA][1] = (dp[i+1][condA][1] + cur * (t[i] - '0')) % MOD;
-
-                    // equal
-                    dp[i+1][condA][condB] = (dp[i+1][condA][condB] + cur) % MOD;
-                }
-                else if(t[i] == '?'){
-                    // s > t
-                    dp[i+1][1][condB] = (dp[i+1][1][condB] + cur * (s[i] - '0')) % MOD;
-
-                    // s < t
-                    dp[i+1][condA][1] = (dp[i+1][condA][1] + cur * ('9' - s[i])) % MOD;
-
-                    // equal
-                    dp[i+1][condA][condB] = (dp[i+1][condA][condB] + cur) % MOD;
-                }
-                else{
-                    int newA = condA || (s[i] > t[i]);
-                    int newB = condB || (s[i] < t[i]);
-
-                    dp[i+1][newA][newB] = (dp[i+1][newA][newB] + cur) % MOD;
-                }
-            }
+    if(s[i] != '?' && t[i] != '?'){
+        if(s[i] > t[i]) dp[i][x][y] = (rec(i+1,1,y));
+        else if(s[i] < t[i]) dp[i][x][y] = (rec(i+1,x,1));
+        else dp[i][x][y] = (rec(i+1,x,y));
+    }
+    else{
+        dp[i][x][y] = 0;
+        if(s[i] == '?' && t[i] == '?'){
+            dp[i][x][y] = (dp[i][x][y] + (45*(rec(i+1,1,y))) % mod) % mod;
+            dp[i][x][y] = (dp[i][x][y] + (45*(rec(i+1,x,1))) % mod) % mod;
+            dp[i][x][y] = (dp[i][x][y] + (10*(rec(i+1,x,y))) % mod) % mod;
+        }
+        else if(s[i] == '?') {
+            int k = t[i] - '0';
+            dp[i][x][y] = (dp[i][x][y] + ((9 - k)*(rec(i+1,1,y))) % mod) % mod;
+            dp[i][x][y] = (dp[i][x][y] + (k*(rec(i+1,x,1))) % mod) % mod;
+            dp[i][x][y] =  (dp[i][x][y] + ((rec(i+1,x,y))) % mod) % mod;
+        }
+        else if(t[i] == '?') {
+            int k = s[i] - '0';
+            dp[i][x][y] = (dp[i][x][y] + (k*(rec(i+1,1,y))) % mod) % mod;
+            dp[i][x][y] = (dp[i][x][y] + ((9 - k)*(rec(i+1,x,1))) % mod) % mod;
+            dp[i][x][y] =  (dp[i][x][y] + ((rec(i+1,x,y))) % mod) % mod;
         }
     }
 
-    cout << dp[n][1][1] << "\n";
+    return dp[i][x][y];
+}
+
+void solve(){
+    cin >> n;
+    cin >> s >> t;
+
+    memset(dp, -1, sizeof(dp));
+
+    cout << rec(0,0,0) << nl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    while(t--){
+        solve();
+    }
+    return 0;
 }
