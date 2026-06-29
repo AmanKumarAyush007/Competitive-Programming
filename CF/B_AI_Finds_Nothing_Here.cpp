8fd479f4136283b4b35d3fc734a0056d21c3467d
AmanKumarAyush007 <amanedu55699@gmail.com>
2026-06-29 19:26:00 +0530

    29-06-2026 19:26:00

---
+++ CF/B_AI_Finds_Nothing_Here.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h" 
+#else
+#define debug(x...)
+#endif
+#define int              int64_t
+#define ff               first
+#define ss               second
+#define pb               push_back
+#define inf              (int)1e18
+#define nl               '\n'
+#define all(a)           (a).begin(),(a).end()
+#define rall(a)          (a).rbegin(),(a).rend()
+#define sm(v)            accumulate(all(v),0LL)
+#define inp(v)           for(auto& x : v) cin >> x;
+#define setbit(x)        __builtin_popcountll(x)
+#define lg(x)            (63 - __builtin_clzll(x)) //log base 2
+#define prefixsum(a)     partial_sum(all(a), (a).begin());
+#define suffixsum(a)     partial_sum(rall(a), (a).rbegin());
+
+
+const int MOD = 998244353;
+
+vector<int> fact;
+vector<int> inv_fact;
+
+int power(int base, int exp, int mod) {
+    int res = 1;
+    base %= mod;
+    while (exp > 0) {
+        if (exp % 2 == 1) res = (res * base) % mod;
+        base = (base * base) % mod;
+        exp /= 2; 
+    }
+    return res;
+}
+int modInverse(int n, int mod) {
+    return power(n, mod - 2, mod);
+}
+void precompute(int n) { 
+    fact.resize(n + 1);
+    inv_fact.resize(n + 1);
+
+    fact[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        fact[i] = (fact[i - 1] * i) % MOD;
+    }
+
+    inv_fact[n] = modInverse(fact[n], MOD);
+    for (int i = n - 1; i >= 0; i--) {
+        inv_fact[i] = (inv_fact[i + 1] * (i + 1)) % MOD;
+    }
+}
+int nCr(int n, int r) {
+    if (n < 0 || r < 0 || r > n) return 0;
+    return (fact[n] * inv_fact[r] % MOD * inv_fact[n - r]) % MOD;
+}
+
+
+void solve() {
+    int n,m, r, c;
+    cin >> n >> m >> r >> c;
+
+    int dx = n-r+1;
+    int dy = m-c+1;
+
+    int x = (n*m) - (dx*dy);
+
+    cout << power(2,x,MOD) << nl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int max_n = 100000;
+    precompute(max_n); 
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ CF/E_It_All_Went_Sideways.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h" 
+#else
+#define debug(x...)
+#endif
+#define int              int64_t
+#define ff               first
+#define ss               second
+#define pb               push_back
+#define inf              (int)1e18
+#define nl               '\n'
+#define all(a)           (a).begin(),(a).end()
+#define rall(a)          (a).rbegin(),(a).rend()
+#define sm(v)            accumulate(all(v),0LL)
+#define inp(v)           for(auto& x : v) cin >> x;
+#define setbit(x)        __builtin_popcountll(x)
+#define lg(x)            (63 - __builtin_clzll(x)) //log base 2
+#define prefixsum(a)     partial_sum(all(a), (a).begin());
+#define suffixsum(a)     partial_sum(rall(a), (a).rbegin());
+
+
+
+void solve(){
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    inp(v);
+
+    vector<int> suff = v;
+    for(int i = n-2; i >= 0; i--){
+        suff[i] = min(v[i], suff[i+1]);    
+    }
+
+
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        ans += v[i] - suff[i];
+    }
+
+
+    int cnt = 0, best = 0;
+    for(int i = 0; i < n-1; i++){
+        if(suff[i] == suff[i+1]) cnt++;
+        else{
+            best = max(cnt, best);
+            cnt = 0;
+        }
+    }
+
+    best = max(best, cnt);
+
+    cout << ans + best << nl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
