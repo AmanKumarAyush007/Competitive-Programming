3f4295dee518dc5fcee573e9a3b08f7ea460d3f7
AmanKumarAyush007 <amanedu55699@gmail.com>
2026-07-01 13:03:05 +0530

    01-07-2026 13:03:04

---
+++ CF/F_A_Bit_Odd.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h" 
+#else
+#define debug(x...)
+#endif
+#define int              int64_t
+#define ff               first
+#define ss               second
+#define pb               push_back
+#define inf              (int)1e18
+#define nl               '\n'
+#define all(a)           (a).begin(),(a).end()
+#define rall(a)          (a).rbegin(),(a).rend()
+#define sm(v)            accumulate(all(v),0LL)
+#define inp(v)           for(auto& x : v) cin >> x;
+#define setbit(x)        __builtin_popcountll(x)
+#define lg(x)            (63 - __builtin_clzll(x)) //log base 2
+#define prefixsum(a)     partial_sum(all(a), (a).begin());
+#define suffixsum(a)     partial_sum(rall(a), (a).rbegin());
+
+
+
+void solve(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int sum = 0, cnt = 0;
+    for(int i = 0; i < n; i++){
+        if(s[i] == '0') sum += cnt;
+        else cnt++;
+    }
+
+    if(sum&1){
+        cout << "Alice\n";
+        return;
+    }
+
+    cnt = 0;
+    for(int i = 0; i < n; i++){
+        if(s[i] == '0' && cnt&1){
+            cout << "Alice\n";
+            return;
+        }        
+        if(s[i] == '1') cnt++;
+    }
+
+    cnt = 0;
+    for(int i = n-1; i >= 0; i--){
+        if(s[i] == '1' && cnt&1){
+            cout << "Alice\n";
+            return;
+        }        
+        if(s[i] == '0') cnt++;
+    }
+
+    cout <<  "Bob" << nl;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
